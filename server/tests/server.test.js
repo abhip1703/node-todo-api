4bf1fb1c964d6b8da0caac389782657e8c077105
Abhishek Paradkar <abhishek@Abhisheks-MacBook-Air.local>
2018-06-26 18:43:20 +0530

    login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,21 +4,17 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
+// const todos = [{
+//   _id: new ObjectID(),
+//   text: 'First test todo'
+// }, {
+//   _id: new ObjectID(),
+//   text: 'Second test todo'
+// }];
 
-const todos = [{
-  _id: new ObjectID(),
-  text: 'First test todo'
-}, {
-  _id: new ObjectID(),
-  text: 'Second test todo'
-}];
-
-beforeEach((done) => {
-  Todo.remove({}).then(() => {
-    return Todo.insertMany(todos);
-  }).then(() => done());
-});
-
+beforeEach(populateTodos);
+beforeEach(populateUsers);
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
     var text = 'Test todo text';
@@ -117,7 +113,7 @@ describe('DELETE /todos/:id', () => {
         }
 
         Todo.findById(hexId).then((todo) => {
-          expect(todo).toNotExist();
+          expect(todo).toBeFalsy();
           done();
         }).catch((e) => done(e));
       });
